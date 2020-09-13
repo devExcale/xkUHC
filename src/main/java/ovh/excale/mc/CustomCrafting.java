5c25e07682a0d992faf072773e899397bdb9145b
Excale <dev_excale@hotmail.com>
2020-09-13 18:28:33 +0200

    Optimise imports

--- src/main/java/ovh/excale/mc/CustomCrafting.java
+++ src/main/java/ovh/excale/mc/CustomCrafting.java
@@ -1,8 +1,5 @@
 package ovh.excale.mc;
 
-import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.ShapelessRecipe;
-
 public abstract class CustomCrafting {
 
 	protected boolean shaped;
