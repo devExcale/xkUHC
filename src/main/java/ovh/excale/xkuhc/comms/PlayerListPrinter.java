5828c0f32cb3ec08fb6f4c97ea8850104dda88ae
Emanuele Scaccia <dev_excale@hotmail.com>
2022-06-18 11:20:55 +0200

    [refactor] Added PlayerList packet wrapper for more readability

--- src/main/java/ovh/excale/xkuhc/comms/PlayerListPrinter.java
+++ src/main/java/ovh/excale/xkuhc/comms/PlayerListPrinter.java
@@ -1,8 +1,6 @@
 package ovh.excale.xkuhc.comms;
 
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.packetwrapper.WrapperPlayServerPlayerListHeaderFooter;
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitTask;
@@ -14,7 +12,6 @@ import ovh.excale.xkuhc.xkUHC;
 import java.util.function.Function;
 import java.util.logging.Logger;
 
-import static com.comphenix.protocol.PacketType.Play.Server.PLAYER_LIST_HEADER_FOOTER;
 import static java.util.logging.Level.WARNING;
 
 public class PlayerListPrinter {
@@ -69,27 +66,24 @@ public class PlayerListPrinter {
 
 	public void update() {
 
-		ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
-		Logger log = xkUHC.instance()
-				.getLogger();
-
 		for(Gamer gamer : hub.getGamers()) {
 
 			WrappedChatComponent header = WrappedChatComponent.fromLegacyText((headerProvider != null) ? headerProvider.apply(gamer) : "");
 			WrappedChatComponent footer = WrappedChatComponent.fromLegacyText((footerProvider != null) ? footerProvider.apply(gamer) : "");
 
-			PacketContainer packet = new PacketContainer(PLAYER_LIST_HEADER_FOOTER);
+			WrapperPlayServerPlayerListHeaderFooter packet = new WrapperPlayServerPlayerListHeaderFooter();
 
-			packet.getChatComponents()
-					.write(0, header)
-					.write(1, footer);
+			packet.setHeader(header);
+			packet.setFooter(footer);
 
 			try {
 
-				protocolManager.sendServerPacket(gamer.getPlayer(), packet);
+				packet.sendPacket(gamer.getPlayer());
 
 			} catch(Exception e) {
-				log.log(WARNING, e.getMessage(), e);
+				xkUHC.instance()
+						.getLogger()
+						.log(WARNING, e.getMessage(), e);
 			}
 
 		}
