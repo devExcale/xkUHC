12b4135182642383eeda90a23adf4d1bef05d1fb
Emanuele Scaccia <dev_excale@hotmail.com>
2022-06-18 11:15:49 +0200

    [misc] Removed unused commands

--- src/main/java/ovh/excale/xkuhc/commands/TestCommand.java
+++ src/main/java/ovh/excale/xkuhc/commands/TestCommand.java
@@ -52,63 +52,6 @@ public class TestCommand {
 
 	}
 
-	@Subcommand("gen")
-	public static void genWorldAndTp(Player player, @APlayerArgument Player otherPlayer) {
-
-		player.sendMessage("Starting generation");
-
-		World world = new WorldManager(log()).loadSpawn(false)
-				.generateUntilClearCenter()
-				.applyRules()
-				.getWorld();
-
-		player.sendMessage("World generated, commencing teleport");
-
-		PlayerSpreader spreader = new PlayerSpreader(world, 1000);
-
-		spreader.spread(player);
-		spreader.spread(otherPlayer);
-
-		player.sendMessage("Teleported");
-
-	}
-
-	@Subcommand("gen")
-	public static void genWorldAndTp(Player player) {
-
-		player.sendMessage("Starting generation");
-
-		World world = new WorldManager(log()).loadSpawn(false)
-				.generateUntilClearCenter()
-				.applyRules()
-				.getWorld();
-
-		player.sendMessage("World generated, commencing teleport");
-
-		PlayerSpreader spreader = new PlayerSpreader(world, 1000);
-		spreader.spread(player);
-
-		player.sendMessage("Teleported");
-
-	}
-
-	@Subcommand("jump")
-	public static void jump(Player player) {
-
-		Bukkit.getScheduler()
-				.runTaskAsynchronously(xkUHC.instance(), () -> {
-
-					World world = new WorldManager(log()).loadSpawn(false)
-							.generateUntilClearCenter()
-							.applyRules()
-							.getWorld();
-
-					new PlayerSpreader(world, 2000).spread(player);
-
-				});
-
-	}
-
 	@Subcommand("sound")
 	public static void sound(Player player, @ASoundArgument Sound sound, @AFloatArgument float pitch, @AFloatArgument float volume) {
 		player.playSound(player, sound, volume, pitch);
